a6f567010a975bd7795f89c27103ef64f4cd96f5
JBondeson19 <jpbond58@gmail.com>
2021-01-26 22:36:30 -0500

    "modified posts - no more title"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_162539) do
+ActiveRecord::Schema.define(version: 2021_01_27_033536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_01_26_162539) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
