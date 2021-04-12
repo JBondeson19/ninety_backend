9b1e1f33d18f75e61dd56a7581bb4bcb92235743
James Bondeson <jpbond58@gmail.com>
2021-04-12 16:58:06 -0400

    schema version update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_203452) do
+ActiveRecord::Schema.define(version: 2021_02_25_162926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,20 +51,17 @@ ActiveRecord::Schema.define(version: 2021_02_22_203452) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "image_elements", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "tags"
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.bigint "users_id", null: false
-    t.string "photo"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["users_id"], name: "index_profiles_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,5 +74,4 @@ ActiveRecord::Schema.define(version: 2021_02_22_203452) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "profiles", "users", column: "users_id"
 end
