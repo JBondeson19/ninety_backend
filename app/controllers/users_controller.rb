0b4d81a4b65a2307f5286379ab5d78c05a0690e4
James Bondeson <jpbond58@gmail.com>
2021-04-25 18:03:08 -0400

    auth reconfiguration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,59 +1,16 @@
 class UsersController < ApplicationController
-    
+    has_secure_password
+
+    validates :username, :email, :password, presence: {message: "Attribute must be given"} on: :create
+    validates :email, uniqueness: true on: :create
+    validates :password, length: {minimum: 6}
+
     def create
-     @user = User.new(user_params)
-         if @user.save
-             login!  
-             render json: {
-             status: :created,
-             user: @user
-         }
-        else 
-            render json: {
-            status: 500,
-            errors: @user.errors.full_messages
-        }
-        end
+        @user = User.create(user_params)
+        
     end
 
-
-    def index
-        @users = User.all
-           if @users
-              render json: {
-              users: @users
-           }
-          else
-              render json: {
-              status: 500,
-              errors: ['no users found']
-          }
-         end
-    end
-
-    def show
-        @user = User.find(params[:id])
-            if @user
-                render json: {
-                user: @user
-            }
-            else
-                render json: {
-                status: 500,
-                errors: ['user not found']
-                }
-            end
-    end
-
-    def update
-        @user = User.find(params[:id])
-            if @user.avatar.attached? 
-                @user.update(user_params)
-            elsif
-                @user.avatar.attach(io: File.open("/path/to/face.jpg"), filename: "face.jpg", content_type: "image/jpg")
-            end
-    end
-
+  
 
     private
 
