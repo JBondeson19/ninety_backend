5538cd5ca8f24aea2de267e015be94610c40510a
James Bondeson <jpbond58@gmail.com>
2021-04-25 21:33:43 -0400

    cleaned up Post Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,12 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-        render json: @posts.to_json(include: :user)
+        render json: @posts
     end
 
     def show
         @post = Post.find(params[:id])
-        render json: @post.to_json(include: :user)
+        render json: @post.to_json(except: [:created_at, :updated_at])
     end
 
     
@@ -17,16 +17,16 @@ class PostsController < ApplicationController
         if @post.save
             render json: @post
         else
-            render json: {error: @post.errors.messages}
+            render json: {error: @post.errors.full_messages}
         end
     end
 
     def edit
         @post = Post.find(params[:id])
-        if @post.destroy
-            p "post destroyed"
+        if @post.update(post_params)
+           render json: @post
         else
-            render json: {error: @post.errors.messages}
+            render json: {error: @post.errors.full_messages}
         end
     end
 
@@ -35,13 +35,13 @@ class PostsController < ApplicationController
         if @post.destroy
             p "post destroyed"
         else
-            render json: {error: @post.errors.messages}
+            render json: {error: @post.errors.full_messages}
         end
     end
 
     private
 
     def post_params
-        params.require(:post).permit!
+        params.require(:post).permit(:tags)
     end
 end
